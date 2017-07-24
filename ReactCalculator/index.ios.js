1ebdb73272d4a18f4b2493a19b6b450ef2d23814
stac <jacob.benjamin.stachowicz@gmail.com>
2017-07-25 00:00:16 +0200

    update

--- ReactCalculator/index.ios.js
+++ ReactCalculator/index.ios.js
@@ -1,6 +1,19 @@
-// What we are doing here is that we are importing React and Component from the react package
+// src/ReactCalculator.js
+
 import React, { Component } from 'react';
 import {
     Text,
     AppRegistry
-} from 'react-native'
+} from 'react-native';
+
+class ReactCalculator extends Component {
+
+    render() {
+        return (
+            <Text>Hello, Rdafadeact!</Text>
+        )
+    }
+
+}
+
+AppRegistry.registerComponent('ReactCalculator', () => ReactCalculator);
