7ed06cacd21d52dbb66513f061f604f653b90059
stac <jacob.benjamin.stachowicz@gmail.com>
2017-07-25 11:54:10 +0200

    added a StyleSheet js file

---
+++ ReactCalculator/src/Style.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Jacob on 2017-07-25.
+ */
+import { StyleSheet } from 'react-native';
+
+var Style = StyleSheet.create({
+    rootContainer: {
+        flex: 1
+    },
+
+    displayContainer: {
+        flex: 2,
+        backgroundColor: '#19412d'
+    },
+
+    inputContainer: {
+        flex: 8,
+        backgroundColor: '#3e6f56'
+    }
+
+
+})
+
+export default Style;
